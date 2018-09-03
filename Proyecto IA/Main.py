c32ab4e018924f6b6c7d644d0344f9aae3e584ef
pablomf97 <pablomf@hotmail.es>
2018-09-03 17:45:34 +0200

    To comentao jej
    Casi todo jej

--- Proyecto IA/Main.py
+++ Proyecto IA/Main.py
@@ -1,13 +1,13 @@
 import warnings
-import matplotlib.pyplot as dibuj;
-from AG import *;
+import matplotlib.pyplot as dibuj
+from AG import *
 
 warnings.filterwarnings(action='ignore')
 
-colores_nodos = enfriamiento_simulado(100, 0.95, 100, 100000)
+colores_nodos = enfriamiento_simulado(100, 0.95, 100, 1000)
 
 nx.draw(g, with_labels=True, node_color=colores_nodos)
 
 print('Para el grafo representado en pantalla, se ha llegado a la conclusión que un coloreado óptimo es el siguiente:\n' + str(colores_nodos) + '\ndonde el valor indica el color en la lista y el índice indica el vértice correspondiene en el grafo, utilizando ' + str(len(set(colores_nodos))) + ' colores')
 
-dibuj.show();
+dibuj.show()
